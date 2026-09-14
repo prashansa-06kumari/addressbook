883e3fc7ab1f061d5f6b2b4d826a5f5db54e19d0
prashansa-06kumari <prashansakumari23@gmail.com>
2026-09-14 11:30:39 +0530

    UC1: create Contact class with ToString

---
+++ src/AddressBookApp/Program.cs
@@ -0,0 +1,4 @@
+using AddressBookApp.Models;
+
+var contact = new Contact("John", "Doe", "12 MG Road", "Pune", "Maharashtra", "411001", "9876543210", "john.doe@mail.com");
+Console.WriteLine(contact.ToString());
